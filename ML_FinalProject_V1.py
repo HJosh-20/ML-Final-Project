72eef07e135302c918ccafdae9366fa005609059
Joshua Harrison <73617376+HJosh-20@users.noreply.github.com>
2025-04-17 11:35:50 -0500

    Add files via upload

---
+++ ML_FinalProject_V1.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, mean_squared_error, r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from matplotlib import pyplot as plt
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv('calories_burned_data.csv')
+df['Gender'] = df['Gender'].map({'Male': 0, 'Female': 1})
+
+X = df.iloc[:, :-1] #all but last
+y = df.iloc[:, -1]
+print(X.shape)
+print(y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Standardize for models that need it SVR & KNN
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# SVR Model
+def run_svr(X_train_scaled, X_test_scaled, y_train, y_test):
+    model = SVR()
+    model.fit(X_train_scaled, y_train)
+    preds = model.predict(X_test_scaled)
+    print("\nSVR Results:")
+    print("R2 Score:", r2_score(y_test, preds))
+    print("MSE:", mean_squared_error(y_test, preds))
+
+# Random Forest
+def run_random_forest(X_train, X_test, y_train, y_test):
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+    preds = model.predict(X_test)
+    print("\nRandom Forest Results:")
+    print("R2 Score:", r2_score(y_test, preds))
+    print("MSE:", mean_squared_error(y_test, preds))
+
+# KNN Regressor
+def run_knn(X_train_scaled, X_test_scaled, y_train, y_test):
+    model = KNeighborsRegressor()
+    model.fit(X_train_scaled, y_train)
+    preds = model.predict(X_test_scaled)
+    print("\nKNN Results:")
+    print("R2 Score:", r2_score(y_test, preds))
+    print("MSE:", mean_squared_error(y_test, preds))
+
+# Main: run all models
+if __name__ == "__main__":
+    #need to add user input here
+    run_svr(X_train_scaled, X_test_scaled, y_train, y_test)
+    run_random_forest(X_train, X_test, y_train, y_test)
+    run_knn(X_train_scaled, X_test_scaled, y_train, y_test)
