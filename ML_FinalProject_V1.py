006abf9de3e2deafbda6d9d4a9709d9c4093fd75
KonnorMazyrack <70538568+KonnorMazyrack@users.noreply.github.com>
2025-04-23 17:15:45 -0500

    Update ML_FinalProject_V1.py
    working dataset

--- ML_FinalProject_V1.py
+++ ML_FinalProject_V1.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix, mean_squared_error, r2_score
+from sklearn.metrics import accuracy_score, confusion_matrix, mean_squared_error, r2_score, mean_absolute_error
 from sklearn.neighbors import KNeighborsRegressor
 from matplotlib import pyplot as plt
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 
-df = pd.read_csv('calories_burned_data.csv')
-df['Gender'] = df['Gender'].map({'Male': 0, 'Female': 1})
+df = pd.read_csv('calories.csv')
+df = df.drop(['User_ID', 'Body_Temp'], axis=1)
+df['Gender'] = df['Gender'].map({'male': 0, 'female': 1})
+print(df)
 
 X = df.iloc[:, :-1] #all but last
 y = df.iloc[:, -1]
@@ -31,6 +33,7 @@ def run_svr(X_train_scaled, X_test_scaled, y_train, y_test):
     print("\nSVR Results:")
     print("R2 Score:", r2_score(y_test, preds))
     print("MSE:", mean_squared_error(y_test, preds))
+    print("MAE:", mean_absolute_error(y_test, preds))
 
 # Random Forest
 def run_random_forest(X_train, X_test, y_train, y_test):
@@ -40,6 +43,7 @@ def run_random_forest(X_train, X_test, y_train, y_test):
     print("\nRandom Forest Results:")
     print("R2 Score:", r2_score(y_test, preds))
     print("MSE:", mean_squared_error(y_test, preds))
+    print("MAE:", mean_absolute_error(y_test, preds))
 
 # KNN Regressor
 def run_knn(X_train_scaled, X_test_scaled, y_train, y_test):
@@ -49,6 +53,7 @@ def run_knn(X_train_scaled, X_test_scaled, y_train, y_test):
     print("\nKNN Results:")
     print("R2 Score:", r2_score(y_test, preds))
     print("MSE:", mean_squared_error(y_test, preds))
+    print("MAE:", mean_absolute_error(y_test, preds))
 
 # Main: run all models
 if __name__ == "__main__":
@@ -56,3 +61,4 @@ if __name__ == "__main__":
     run_svr(X_train_scaled, X_test_scaled, y_train, y_test)
     run_random_forest(X_train, X_test, y_train, y_test)
     run_knn(X_train_scaled, X_test_scaled, y_train, y_test)
+    
