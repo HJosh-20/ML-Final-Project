a85cc76ff3c6dc745d4aaeb57346f067c9f39795
Joshua Harrison <73617376+HJosh-20@users.noreply.github.com>
2025-04-29 15:00:28 -0500

    ML_FinalProject_V5.py
    Polynomial function in option 2 is running into an error.

--- ML_FinalProject_V5.py
+++ ML_FinalProject_V5.py
@@ -11,17 +11,18 @@ from sklearn.linear_model import LinearRegression
 import re
 import warnings
 import seaborn as sns
+
 warnings.filterwarnings("ignore")
 
 df = pd.read_csv('calories.csv')
 df['Gender'] = df['Gender'].map({'male': 0, 'female': 1})
-print(df)
-df_test = pd.read_csv('calories_user_data.csv')
+
+userGenDf = pd.read_csv('calories_user_data.csv')
+generatedX = userGenDf.iloc[:, :-1]
+generatedY = userGenDf.iloc[:, -1]
 
 X = df.iloc[:, :-1] #all but last
 y = df.iloc[:, -1]
-print(X.shape)
-print(y.shape)
 
 #need fig here bc it represents the whole figure (the full canvas),
 #and ax is like a specific spot (or subplot) on that canvas
@@ -32,21 +33,21 @@ plt.title("Feature Correlation Heatmap")
 plt.tight_layout()
 plt.show()
 
-print(df.corr()['Calories'].sort_values(ascending=False))
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Standardize for models that need it SVR & KNN
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+#for generated user input
+genX_scaled = scaler.transform(generatedX)
 
 # SVR Model
 def run_svr(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scaled=None):
     model = SVR()
     model.fit(X_train_scaled, y_train)
     preds = model.predict(X_test_scaled)
-    print("\nSVR Results:")
+    print("\n|       SVR Results       |")
     print("R2 Score:", r2_score(y_test, preds))
     print("MSE:", mean_squared_error(y_test, preds))
     print("MAE:", mean_absolute_error(y_test, preds))
@@ -55,13 +56,15 @@ def run_svr(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scal
         print("\nPredicted calories burned (SVR):", round(pred_svr[0], 2))
     if option == 1:
         plottingML(preds, title="SVR", target_var=y_test, algorithm_model=model, features_names=X.columns, test_features=X_test)
+    if option == 2:
+        return model
 
 # Random Forest
 def run_random_forest(X_train, X_test, y_train, y_test, option, user_df=None):
     model = RandomForestRegressor()
     model.fit(X_train, y_train)
     preds = model.predict(X_test)
-    print("\nRandom Forest Results:")
+    print("\n|   Random Forest Results   |")
     print("R2 Score:", r2_score(y_test, preds))
     print("MSE:", mean_squared_error(y_test, preds))
     print("MAE:", mean_absolute_error(y_test, preds))
@@ -70,13 +73,15 @@ def run_random_forest(X_train, X_test, y_train, y_test, option, user_df=None):
         print("\nPredicted calories burned (Random Forest):", round(pred_rf[0], 2))
     if option == 1:
         plottingML(preds, title="RF", target_var=y_test, algorithm_model=model, features_names=X.columns, test_features=X_test)
+    if option == 2:
+        return model
 
 # KNN Regressor
 def run_knn(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scaled=None):
     model = KNeighborsRegressor()
     model.fit(X_train_scaled, y_train)
     preds = model.predict(X_test_scaled)
-    print("\nKNN Results:")
+    print("\n|       KNN Results       |")
     print("R2 Score:", r2_score(y_test, preds))
     print("MSE:", mean_squared_error(y_test, preds))
     print("MAE:", mean_absolute_error(y_test, preds))
@@ -85,16 +90,19 @@ def run_knn(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scal
         print("\nPredicted calories burned (KNN):", round(pred_knn[0], 2))
     if option == 1:
         plottingML(preds, title="KNN", target_var=y_test, algorithm_model=model, features_names=X.columns, test_features=X_test)
+    if option == 2:
+        return model
 
 # Polynomial Regression
 def run_poly(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scaled=None):
+    degree_symbol = "\u00B0"
     for i in range(1, 4):
         poly = PolynomialFeatures(degree = i)
         x_train_poly = poly.fit_transform(X_train_scaled)
         x_test_poly = poly.transform(X_test_scaled)
         model = LinearRegression().fit(x_train_poly, y_train)
         preds = model.predict(x_test_poly)
-        print(f"\nPolynomial Regression (Degree {i}) Results:")
+        print(f"\n|   Polynomial Regression {i}{degree_symbol} Results   |")
         print("R2 Score:", r2_score(y_test, preds))
         print("MSE:", mean_squared_error(y_test, preds))
         print("MAE:", mean_absolute_error(y_test, preds))
@@ -103,8 +111,10 @@ def run_poly(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_sca
             pred_poly = model.predict(user_df_poly)
             print(f"\nPredicted calories burned:", round(pred_poly[0], 2))
         if option == 1:
-            plottingML(preds, title=f"Poly Degree {i}", target_var=y_test, algorithm_model=model, features_names=X.columns, test_features=X_test)
-
+            plottingML(preds, title=f"Polynomial {i}{degree_symbol}", target_var=y_test, algorithm_model=model, features_names=X.columns, test_features=X_test)
+        if option == 2:
+            return model
+   
 def get_height_cm():
     while True:
         height_input = input("Enter your height (e.g., 5 foot 2 inches): ").lower()
@@ -161,7 +171,6 @@ def plottingML(preds, title, target_var=None, algorithm_model=None, features_nam
         plt.grid(True)
         plt.show()
                  
-   
     #imporant features for RF model.
     if title == 'RF':
         important_features = algorithm_model.feature_importances_
@@ -177,86 +186,126 @@ def plottingML(preds, title, target_var=None, algorithm_model=None, features_nam
 
 # Main: run all models
 if __name__ == "__main__":
-    print("1. Original train and test data w/ graphs")
-    print("2. Generated user input data w/ graphs")
-    print("3. User input predictions w/o graphs")
-    option = int(input("Enter the option you would like select: "))
     while True:
-        if option < 1 or option > 3:
-            option = int(input("Invalid option: Please re-enter your option: "))
-        else: 
+        print("-------------------------")
+        print("|        Options        |")
+        print("-------------------------")
+        print("1. Original train and test data w/ graphs")
+        print("2. Generated user input data w/ graphs")
+        print("3. User input predictions w/o graphs")
+        print("4. End Program")
+        option = int(input("Enter the option you would like select: "))
+    
+        while True:
+            if option < 1 or option > 4:
+                option = int(input("Invalid option: Please re-enter your option: "))
+            else: 
+                break
+
+        if option == 3:
+            print("You selected option 3.")
+            #test data inputs height=6 foot 3inches(~190cm), weight=207.2345(~94kg)
+            gender = input("Please enter your gender: ")
+            while True:
+                if   gender.strip().lower() == "male":
+                    gender = 0
+                    break
+                elif gender.strip().lower() == "female":
+                    gender = 1
+                    break
+                else:
+                    gender = input("Invalid gender: Please re-enter your gender: ")
+            age = int(input("Please enter your age: "))
+            while True:
+                if age < 20 or age > 79:
+                    age = int(input("Invalid age: Please re-enter your age: "))
+                else: 
+                    break
+            height_cm = get_height_cm()
+            while True:
+                #from ~4 foot to ~7 foot 3 inches 
+                if height_cm < 123.0 or height_cm > 222.0:
+                    print("Invalid height")
+                    height_cm = get_height_cm()
+                else: 
+                    break
+            print(f"Your height in centimeters is: {height_cm:.2f} cm")
+            weight_kg = get_weight_kg()
+            while True:
+                #from ~79 pounds to ~292 pounds
+                if weight_kg < 36.0 or weight_kg > 132.0:
+                    print("Invalid weight")
+                    weight_kg = get_weight_kg()
+                else: 
+                    break
+            print(f"Your weight in kilograms is: {weight_kg:.2f} kg")
+            duration = float(input("Please enter the duration of your workout: "))
+            while True:
+                if duration < 1 or duration > 30:
+                    duration = float(input("Invalid duration: Please re-enter your duration of your workout: "))
+                else: 
+                    break
+            heart_rate = float(input("please enter your average heart rate during your workout: "))
+            while True:
+                if heart_rate < 67 or heart_rate > 128:
+                    heart_rate = float(input("Invalid average heart rate: Please re-enter your average heart rate during your workout: "))
+                else: 
+                    break
+
+            feature_names = ['Gender', 'Age', 'Height', 'Weight', 'Duration', 'Heart_Rate']
+            user_data = np.array([gender, age, height_cm, weight_kg, duration, heart_rate]).reshape(1,-1)
+            user_df = pd.DataFrame(user_data, columns=feature_names)
+            print("User data (Dataframe): ")
+            print(user_df)
+
+            user_df_scaled = scaler.transform(user_data)
+            print("User data(Dataframe scaled): ", user_df_scaled)
+
+            run_svr(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scaled)
+            run_random_forest(X_train, X_test, y_train, y_test, option, user_df)
+            run_knn(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scaled)
+            run_poly(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scaled)
+            print(" ")
+
+        elif option == 2:
+            print("You selected option 2.")
+            model_svr = run_svr(X_train_scaled, X_test_scaled, y_train, y_test, option)
+            model_rf = run_random_forest(X_train, X_test, y_train, y_test, option)
+            model_knn = run_knn(X_train_scaled, X_test_scaled, y_train, y_test, option)
+            model_poly = run_poly(X_train_scaled, X_test_scaled, y_train, y_test, option)
+
+            generated_preds_svr = model_svr.predict(genX_scaled)
+            generated_preds_rf = model_rf.predict(generatedX)
+            generated_preds_knn = model_knn.predict(genX_scaled)
+            
+            generated_preds_poly = model_poly.predict(user_df_poly)
+
+            plottingML(generated_preds_svr, title="SVR (Generated Users)", target_var=generatedY, algorithm_model=model_svr, features_names=generatedX.columns, test_features=generatedX)
+            plottingML(generated_preds_rf, title="RF (Generated Users)", target_var=generatedY, algorithm_model=model_rf, features_names=generatedX.columns, test_features=generatedX)
+            plottingML(generated_preds_knn, title="KNN (Generated Users)", target_var=generatedY, algorithm_model=model_knn, features_names=generatedX.columns, test_features=generatedX)
+            plottingML(generated_preds_poly, title="Polynomial Regression (Generated Users)", target_var=generatedY, algorithm_model=model_knn, features_names=generatedX.columns, test_features=generatedX)
+            print(" ")
+
+        elif option == 1: 
+            print("You selected option 1.")
+            run_svr(X_train_scaled, X_test_scaled, y_train, y_test, option)
+            run_random_forest(X_train, X_test, y_train, y_test, option)
+            run_knn(X_train_scaled, X_test_scaled, y_train, y_test, option)
+            run_poly(X_train_scaled, X_test_scaled, y_train, y_test, option)
+            #residual boxplots all models
+            plt.figure(figsize=(10, 6))
+            plt.boxplot(model_residuals, labels=model_names)
+            plt.title("Comparison of Residuals Across Models")
+            plt.ylabel("Residuals")
+            plt.grid(True)
+            plt.tight_layout()
+            plt.show()
+            print(" ")
+
+        elif option == 4:
+            print("Goodbye!")
             break
 
-    if option == 3: #done
-        print("You selected option 3.")
-        #test data inputs height=6 foot 3inches(~190cm), weight=207.2345(~94kg)
-        gender = input("Please enter your gender: ")
-        while True:
-            if   gender.strip().lower() == "male":
-                gender = 0
-                break
-            elif gender.strip().lower() == "female":
-                gender = 1
-                break
-            else:
-                gender = input("Invalid gender: Please re-enter your gender: ")
-        age = int(input("Please enter your age: "))
-        while True:
-            if age < 20 or age > 79:
-                age = int(input("Invalid age: Please re-enter your age: "))
-            else: 
-                break
-        height_cm = get_height_cm()
-        while True:
-            if height_cm < 123.0 or height_cm > 222.0:
-                print("Invalid height")
-                height_cm = get_height_cm()
-            else: 
-                break
-        print(f"Your height in centimeters is: {height_cm:.2f} cm")
-        weight_kg = get_weight_kg()
-        while True:
-            if weight_kg < 36.0 or weight_kg > 132.0:
-                print("Invalid weight")
-                weight_kg = get_weight_kg()
-            else: 
-                break
-        print(f"Your weight in kilograms is: {weight_kg:.2f} kg")
-        duration = float(input("Please enter the duration of your workout: "))
-        while True:
-            if duration < 1 or duration > 30:
-                duration = float(input("Invalid duration: Please re-enter your duration of your workout: "))
-            else: 
-                break
-        heart_rate = float(input("please enter your average heart rate during your workout: "))
-        while True:
-            if heart_rate < 67 or heart_rate > 128:
-                heart_rate = float(input("Invalid average heart rate: Please re-enter your average heart rate during your workout: "))
-            else: 
-                break
-
-        feature_names = ['Gender', 'Age', 'Height', 'Weight', 'Duration', 'Heart_Rate']
-        user_data = np.array([gender, age, height_cm, weight_kg, duration, heart_rate]).reshape(1,-1)
-        user_df = pd.DataFrame(user_data, columns=feature_names)
-        print("User data (Dataframe): ")
-        print(user_df)
-
-        user_df_scaled = scaler.transform(user_data)
-        print("User data(Dataframe scaled): ", user_df_scaled)
-
-        run_svr(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scaled)
-        run_random_forest(X_train, X_test, y_train, y_test, option, user_df)
-        run_knn(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scaled)
-        run_poly(X_train_scaled, X_test_scaled, y_train, y_test, option, user_df_scaled)
-    elif option == 2:
-        print("You selected option 2.")
-        run_svr(X_train_scaled, X_test_scaled, y_train, y_test, option)
-        run_random_forest(X_train, X_test, y_train, y_test, option)
-        run_knn(X_train_scaled, X_test_scaled, y_train, y_test, option)
-        run_poly(X_train_scaled, X_test_scaled, y_train, y_test, option)
-    elif option == 1: #done
-        print("You selected option 1.")
-        run_svr(X_train_scaled, X_test_scaled, y_train, y_test, option)
-        run_random_forest(X_train, X_test, y_train, y_test, option)
-        run_knn(X_train_scaled, X_test_scaled, y_train, y_test, option)
-        run_poly(X_train_scaled, X_test_scaled, y_train, y_test, option)
+    
+    
+    
